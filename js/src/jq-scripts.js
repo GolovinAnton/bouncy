200277c11de52904a4623ef465d3d381565819fa
GolovinAnton <idealify@gmail.com>
2018-03-21 21:46:44 +0200

    v3

--- js/src/jq-scripts.js
+++ js/src/jq-scripts.js
@@ -79,7 +79,7 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: address,
         map: map,
-        icon: 'favicon.png',
+        icon: '././img/marker.png',
         animation: google.maps.Animation.DROP
     });
 }
